9ab941d29a75450eac52e76c2bc7edae319de5b8
sohila samir <sohila.samir1911@gmail.com>
2022-04-26 01:56:23 +0200

    final testing for the logic before refactoring into clean code

--- public/app.js
+++ public/app.js
@@ -5,6 +5,7 @@ const clearBtn = document.getElementById('C')
 const deleteBtn = document.getElementById('delete')
 const equalBtn = document.getElementById('equal')
 const pointBtn = document.getElementById('point')
+const currSign = document.getElementById('current-sign')
 
 let signIndicator = 0
 let total = 0
@@ -15,7 +16,8 @@ let secondValue;
 
 
 const clear = () => {
-    result.innerText = ''
+    result.innerText = '0'
+    currSign.innerText = ''
     signIndicator = 0
     secondValue = 0
     firstValue = 0
@@ -27,6 +29,34 @@ clearBtn.addEventListener('click', (e) => {
     clear()
 })
 
+deleteBtn.addEventListener('click', (e) => {
+    try {
+        if (signIndicator && !secondValue) {
+            sign = [...sign].splice(1,1).join('')
+            signIndicator = 0
+            currSign.innerText = ''
+            console.log('current sign: ', sign);
+        } else {
+            if (signIndicator) {
+                secondValue = [...secondValue].splice(0, secondValue.length - 1).join('')
+                console.log(secondValue);
+            } else {
+                firstValue = [...firstValue].splice(0, firstValue.length - 1).join('')
+                console.log(firstValue);
+            }
+            let resultToArray = [...result.innerHTML]
+            let resultWithDeletedElement = resultToArray.splice(0, resultToArray.length - 1)
+            result.innerText = resultWithDeletedElement.join('')
+        }
+
+        if (!result.innerText.length) {
+            result.innerText = '0'
+        }
+    }catch (e) {
+        console.log(`${e.name}:`, e.message);
+    }
+
+})
 
 pointBtn.addEventListener('click', (e) => {
     let resultInterface = result.innerText
@@ -43,39 +73,31 @@ pointBtn.addEventListener('click', (e) => {
 const addNumbers = () => {
     for (let i = 0; i < digits.length; i++) {
         digits[i].addEventListener('click', (e) => {
-            if (result.innerText !== '0' && signIndicator === 0) {
+            if (result.innerText !== '0' && result.innerText !== 'Syntax Error' && signIndicator === 0) {
                 result.innerText += e.target.innerText
-                console.log(1);
             }
             if (signIndicator === 1 && !secondValue) {
                 result.innerText = e.target.innerText
-                console.log(2);
             }
             if (secondValue && signIndicator === 1) {
                 result.innerText += e.target.innerText
-                console.log(3);
             }
-            if (result.innerText === '0') {
+            if (result.innerText === '0' || result.innerText === 'Syntax Error' || (total && signIndicator === 0)) {
                 result.innerText = e.target.innerText
-                console.log(4);
             }
 
             if (signIndicator === 0 && !firstValue) {
                 firstValue = e.target.innerText
                 console.log('numbers taped: ', firstValue);
-                console.log(5);
             } else if (signIndicator === 0 && firstValue) {
                 firstValue += e.target.innerText
                 console.log('numbers taped: ', firstValue);
-                console.log(6);
             } else if (signIndicator === 1 && !secondValue) {
                 secondValue = e.target.innerText
                 console.log('number taped after sign: ', secondValue);
-                console.log(7);
             } else {
                 secondValue += e.target.innerText
                 console.log('number taped after sign: ', secondValue);
-                console.log(8);
             }
         })
     }
@@ -88,6 +110,7 @@ const chooseSign = () => {
         operators[i].addEventListener('click', (e) => {
             sign = e.target.innerText
             signIndicator++
+            currSign.innerText = e.target.innerText
             console.log('sign: ', sign);
             console.log('sign indicator: ', signIndicator);
         })
@@ -100,52 +123,54 @@ const calculateTotal = () => {
     equalBtn.addEventListener('click', (e) => {
         if (signIndicator > 1) {
             clear()
-        }
-
-        if (sign === '+') {
-            if (total) {
-                total += Number(secondValue)
-            } else {
-                total = Number(firstValue) + Number(secondValue)
+            result.innerText = 'Syntax Error'
+        } else {
+            if (sign === '+') {
+                if (total) {
+                    total += Number(secondValue)
+                } else {
+                    total = Number(firstValue) + Number(secondValue)
+                }
             }
-        }
 
-        if (sign === '-') {
-            if (total) {
-                total -= Number(secondValue)
-            } else {
-                total = Number(firstValue) - Number(secondValue)
+            if (sign === '-') {
+                if (total) {
+                    total -= Number(secondValue)
+                } else {
+                    total = Number(firstValue) - Number(secondValue)
+                }
             }
-        }
 
-        if (sign === '/') {
-            if (total) {
-                total /= Number(secondValue)
-            } else {
-                total = Number(firstValue) / Number(secondValue)
+            if (sign === '/') {
+                if (total) {
+                    total /= Number(secondValue)
+                } else {
+                    total = Number(firstValue) / Number(secondValue)
+                }
             }
-        }
 
-        if (sign === '*') {
-            if (total) {
-                total *= Number(secondValue)
-            } else {
-                total = Number(firstValue) * Number(secondValue)
+            if (sign === '*') {
+                if (total) {
+                    total *= Number(secondValue)
+                } else {
+                    total = Number(firstValue) * Number(secondValue)
+                }
             }
-        }
 
-        if (sign === '%') {
-            if (total) {
-                total %= Number(secondValue)
-            } else {
-                total = Number(firstValue) % Number (secondValue)
+            if (sign === '%') {
+                if (total) {
+                    total %= Number(secondValue)
+                } else {
+                    total = Number(firstValue) % Number (secondValue)
+                }
             }
+            result.innerText = total
         }
         signIndicator = 0
         secondValue = 0
         firstValue = 0
-        result.innerText = total
-        console.log(total);
+        currSign.innerText = ''
+        console.log('total: ', total);
     })
 }
 
