99ce78bdac3249f934de0fd7d10e28ef6aa9c1e6
sohila samir <sohila.samir1911@gmail.com>
2022-04-27 19:56:28 +0200

    testing before switching to Classes

--- public/app.js
+++ public/app.js
@@ -7,49 +7,187 @@ const equalBtn = document.getElementById('equal')
 const pointBtn = document.getElementById('point')
 const currSign = document.getElementById('current-sign')
 
-let signIndicator = 0
+// helper variables
+let multiSignPressIndicator = 0
 let total = 0
 let firstValue;
 let sign;
 let secondValue;
 
 
-
-const clear = () => {
-    result.innerText = '0'
+// helper functions
+const reset = (rmTotal = false) => {
+    if (rmTotal) {
+        total = 0
+    }
     currSign.innerText = ''
-    signIndicator = 0
+    multiSignPressIndicator = 0
     secondValue = 0
     firstValue = 0
-    total = 0
+    console.log('reset');
 }
+/* -----------------------------------------------------------------------------------------------------*/
+const removingTechnique = (element, domElement = false) => {
+    let newElement;
+    if (domElement) {
+        newElement = Array.from(element.innerText).splice(0, element.innerText.length - 1).join('')
+    } else {
+        let elementArr = Array.from((element).toString())
+        newElement = elementArr.splice(0, elementArr.length - 1).join('')
+    }
+    return newElement
+}
+/* -----------------------------------------------------------------------------------------------------*/
 
+const removeElementFromResult = () => {
+    result.innerText = removingTechnique(result, true)
+}
+/* -----------------------------------------------------------------------------------------------------*/
+
+const checkFirstValue = () => {
+    if (firstValue && !currSign.innerText.length) {
+        firstValue = removingTechnique(firstValue)
+        console.log('current first Value:', firstValue);
+        return true
+    } else {
+        console.log('not a first value case');
+        return false
+    }
+}
+/* -----------------------------------------------------------------------------------------------------*/
+
+const checkSign = () => {
+    if (currSign.innerText.length && !secondValue) {
+        currSign.innerText = ''
+        sign = ''
+        multiSignPressIndicator--
+        console.log('current sign: ', sign);
+        return true
+    } else {
+        console.log('not a sign case');
+        return false
+    }
+}
+/* -----------------------------------------------------------------------------------------------------*/
+
+const checkSecondValue = () => {
+    if (secondValue && currSign.innerText.length) {
+        secondValue = removingTechnique(secondValue)
+        console.log('current second Value:', secondValue);
+        return true
+    } else {
+        console.log('not a second value case');
+        return false
+    }
+}
+/* -----------------------------------------------------------------------------------------------------*/
+
+const checkTotal = () => {
+    if (total && !secondValue && !currSign.innerText.length) {
+        removeElementFromResult()
+        total = result.innerText
+        console.log('current total:', total);
+        return true
+    } else {
+        console.log('not a total case');
+        return false
+    }
+}
+/* -----------------------------------------------------------------------------------------------------*/
+
+const appendOrAdd = (element, e, append = false) => {
+    if (append) {
+        return element.innerText += e.target.innerText
+    } else {
+        return element.innerText = e.target.innerText
+    }
+}
+/* -----------------------------------------------------------------------------------------------------*/
+
+const checkTotalToAppendToTotal = (e) => {
+    if (total && !secondValue && !currSign.innerText.length ) {
+        total.toString()
+        total += e.target.innerText
+        appendOrAdd(result, e, true)
+        return true
+    } else {
+        console.log('not a total digit case');
+        return false
+    }
+}
+/* -----------------------------------------------------------------------------------------------------*/
+
+const checkSignToAddFirstValue = (e) => {
+    if (
+        (!currSign.innerText.length && !firstValue) ||
+        result.innerText === 'Syntax Error'
+    ) {
+        appendOrAdd(result, e)
+        firstValue = e.target.innerText
+        console.log('first',1);
+        return true
+    }
+
+    if (!currSign.innerText.length && firstValue) {
+        appendOrAdd(result, e, true)
+        firstValue += e.target.innerText
+        console.log('first',2);
+        return true
+    } else {
+        console.log('not a first value digit case');
+        return false
+    }
+}
+/* -----------------------------------------------------------------------------------------------------*/
+
+const checkSignToAddSecondValue = (e) => {
+    if ((currSign.innerText.length && !secondValue && result.innerText !== '.')) {
+        appendOrAdd(result, e)
+        console.log('second',1);
+        secondValue = e.target.innerText
+        return true
+    }
+
+    if (
+        (currSign.innerText.length && secondValue) ||
+        (currSign.innerText.length && !secondValue) ||
+        (total && !currSign.innerText.length)
+    ) {
+        appendOrAdd(result, e, true)
+        console.log('second',2);
+        secondValue += e.target.innerText
+        return true
+    } else {
+        console.log('not a second value digit case');
+        return false
+    }
+}
+/* -----------------------------------------------------------------------------------------------------*/
+//                                        END OF HELPER FUNCTIONS
+/* -----------------------------------------------------------------------------------------------------*/
 
 clearBtn.addEventListener('click', (e) => {
-    clear()
+    reset(true)
+    result.innerText = '0'
 })
+/* -----------------------------------------------------------------------------------------------------*/
 
 deleteBtn.addEventListener('click', (e) => {
     try {
-        if (signIndicator && !secondValue) {
-            sign = [...sign].splice(1,1).join('')
-            signIndicator = 0
-            currSign.innerText = ''
-            console.log('current sign: ', sign);
-        } else {
-            if (signIndicator) {
-                secondValue = [...secondValue].splice(0, secondValue.length - 1).join('')
-                console.log(secondValue);
+        if (!checkSign() && result.innerText !== 'Syntax Error') {
+            if (checkTotal()) {
+                return
             } else {
-                firstValue = [...firstValue].splice(0, firstValue.length - 1).join('')
-                console.log(firstValue);
+                checkFirstValue()
+                checkSecondValue()
+                removeElementFromResult()
             }
-            let resultToArray = [...result.innerHTML]
-            let resultWithDeletedElement = resultToArray.splice(0, resultToArray.length - 1)
-            result.innerText = resultWithDeletedElement.join('')
+        } else {
+            checkSign()
         }
 
-        if (!result.innerText.length) {
+        if (!result.innerText.length && !secondValue && !firstValue && !total) {
+            reset(true)
             result.innerText = '0'
         }
     }catch (e) {
@@ -57,121 +195,118 @@ deleteBtn.addEventListener('click', (e) => {
     }
 
 })
+/* -----------------------------------------------------------------------------------------------------*/
 
 pointBtn.addEventListener('click', (e) => {
     let resultInterface = result.innerText
-    if (!resultInterface.match(/[.]/)) {
-        result.innerText += e.target.innerText
-        if (signIndicator) {
+    if (
+        result.innerText !== 'Syntax Error' &&
+        !resultInterface.match(/[.]/) &&
+        (total).toString().length > 0
+    ) {
+        if (!secondValue && currSign.innerText.length) {
+            secondValue = e.target.innerText
+            console.log('second value:',secondValue);
+        } else if (currSign.innerText.length) {
             secondValue += e.target.innerText
+            console.log('second value:',secondValue);
+        } else if (total) {
+            total += e.target.innerText
         } else {
             firstValue += e.target.innerText
+            console.log('first value:',secondValue);
         }
+        result.innerText += e.target.innerText
     }
 })
+/* -----------------------------------------------------------------------------------------------------*/
 
 const addNumbers = () => {
     for (let i = 0; i < digits.length; i++) {
         digits[i].addEventListener('click', (e) => {
-            if (result.innerText !== '0' && result.innerText !== 'Syntax Error' && signIndicator === 0) {
-                result.innerText += e.target.innerText
-            }
-            if (signIndicator === 1 && !secondValue) {
-                result.innerText = e.target.innerText
-            }
-            if (secondValue && signIndicator === 1) {
-                result.innerText += e.target.innerText
-            }
-            if (result.innerText === '0' || result.innerText === 'Syntax Error' || (total && signIndicator === 0)) {
-                result.innerText = e.target.innerText
-            }
-
-            if (signIndicator === 0 && !firstValue) {
-                firstValue = e.target.innerText
-                console.log('numbers taped: ', firstValue);
-            } else if (signIndicator === 0 && firstValue) {
-                firstValue += e.target.innerText
-                console.log('numbers taped: ', firstValue);
-            } else if (signIndicator === 1 && !secondValue) {
-                secondValue = e.target.innerText
-                console.log('number taped after sign: ', secondValue);
-            } else {
-                secondValue += e.target.innerText
-                console.log('number taped after sign: ', secondValue);
+            if (checkTotalToAppendToTotal(e)) {
+                console.log('total:', total);
+            } else if (checkSignToAddFirstValue(e)) {
+                console.log('first value:', firstValue);
+            } else if (checkSignToAddSecondValue(e)) {
+                console.log('second value:', secondValue);
             }
         })
     }
 }
 
 addNumbers()
+/* -----------------------------------------------------------------------------------------------------*/
 
 const chooseSign = () => {
     for (let i = 0; i < operators.length; i++) {
         operators[i].addEventListener('click', (e) => {
-            sign = e.target.innerText
-            signIndicator++
-            currSign.innerText = e.target.innerText
-            console.log('sign: ', sign);
-            console.log('sign indicator: ', signIndicator);
+            if (result.innerText !== 'Syntax Error') {
+                currSign.innerText = e.target.innerText
+                sign = e.target.innerText
+                multiSignPressIndicator++
+                console.log('sign: ', sign);
+                console.log('multi sign press indicator: ', multiSignPressIndicator);
+            }
         })
     }
 }
 
 chooseSign()
+/* -----------------------------------------------------------------------------------------------------*/
 
 const calculateTotal = () => {
     equalBtn.addEventListener('click', (e) => {
-        if (signIndicator > 1) {
-            clear()
+        if (multiSignPressIndicator > 1) {
+            reset(true)
             result.innerText = 'Syntax Error'
         } else {
+            total = Number(total)
             if (sign === '+') {
                 if (total) {
-                    total += Number(secondValue)
+                    secondValue ? total += Number(secondValue) :  result.innerText = 'Syntax Error'
                 } else {
-                    total = Number(firstValue) + Number(secondValue)
+                    firstValue && secondValue ? total = Number(firstValue) + Number(secondValue) : result.innerText = 'Syntax Error'
                 }
             }
 
             if (sign === '-') {
                 if (total) {
-                    total -= Number(secondValue)
+                    secondValue ? total -= Number(secondValue) : result.innerText = 'Syntax Error'
                 } else {
-                    total = Number(firstValue) - Number(secondValue)
+                    firstValue && secondValue ? total = Number(firstValue) - Number(secondValue) : result.innerText = 'Syntax Error'
                 }
             }
 
             if (sign === '/') {
                 if (total) {
-                    total /= Number(secondValue)
+                    secondValue ? total /= Number(secondValue) : result.innerText = 'Syntax Error'
                 } else {
-                    total = Number(firstValue) / Number(secondValue)
+                    firstValue && secondValue ? total = Number(firstValue) / Number(secondValue) : result.innerText = 'Syntax Error'
                 }
             }
 
             if (sign === '*') {
                 if (total) {
-                    total *= Number(secondValue)
+                    secondValue ? total *= Number(secondValue) : result.innerText = 'Syntax Error'
                 } else {
-                    total = Number(firstValue) * Number(secondValue)
+                    firstValue && secondValue ? total = Number(firstValue) * Number(secondValue) : result.innerText = 'Syntax Error'
                 }
             }
 
             if (sign === '%') {
                 if (total) {
-                    total %= Number(secondValue)
+                    secondValue ? total %= Number(secondValue) : result.innerText = 'Syntax Error'
                 } else {
-                    total = Number(firstValue) % Number (secondValue)
+                    firstValue && secondValue ? total = Number(firstValue) % Number (secondValue) : result.innerText = 'Syntax Error'
                 }
             }
+            reset()
             result.innerText = total
+            console.log('total: ', total);
         }
-        signIndicator = 0
-        secondValue = 0
-        firstValue = 0
-        currSign.innerText = ''
-        console.log('total: ', total);
     })
 }
 
 calculateTotal()
+/* -----------------------------------------------------------------------------------------------------*/
